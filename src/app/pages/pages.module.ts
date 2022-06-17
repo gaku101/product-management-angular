13deb05c472c8a6b5bba85b07aa37a04deff0665
takehiro <takehiro-r133@hotmail.co.jp>
2022-06-17 21:07:46 +0900

    add reactive forms

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import {
     ProductListingPageComponent
@@ -25,7 +27,7 @@ import {
     StockRegisteringPageComponent,
     PurchaseHistoryListingPageComponent
   ],
-  imports: [CommonModule, HttpClientModule, NgxTranslateModule],
+  imports: [CommonModule, HttpClientModule, MaterialModule, NgxTranslateModule, ReactiveFormsModule],
   exports: [
     SignInPageComponent,
     ProductListingPageComponent,
